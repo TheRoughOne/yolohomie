210addb0c3ab385312b31f534500e8ce4cfc214d
TheRoughOne <azraf@veysantis.com>
2017-07-25 19:25:12 -0400

    Add files via upload

---
+++ yolohomie/yolohomie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  yolohomie
+//
+//  Created by Azraf Ullah on 7/25/17.
+//  Copyright © 2017 Azraf Ullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
